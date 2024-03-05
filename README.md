d55257b796fb116464d0deb9b7a3d02eece86fc8
Vitor de Paula <vitor.de.paula@consultant.volvo.com>
2024-03-05 14:26:56 +0100

    update run-multiple, adds README.md
    Signed-off-by: Vitor de Paula <vitor.de.paula@consultant.volvo.com>

---
+++ README.md
@@ -0,0 +1,12 @@
+**Linux (Ubuntu) instructions:**
+
+Compile with:
+`mcs ttt3d.cs`
+or the C# compiler of your preference
+
+Execute with:
+`mono ttt3d.exe`
+
+Extra:
+If you have **byobu** installed, the run-multiple.sh script opens a 2x2 grid
+of splits and run 4 copies of the executable (you must have compiled it first)
--- run-multiple.sh
+++ run-multiple.sh
@@ -1,21 +1,26 @@
-#Select default pane. Probably an unnecessary line of code
-byobu select-pane -t 0
+#!/usr/bin/env bash
 
-#Split pane 0 into two vertically stacked panes
+# Check byobu available
+byobu --version 2>/dev/null || { echo "byobu not available"; exit 1; }
+
+# Delete any extra panes from current window
+byobu kill-pane -a
+
+# Split pane 0 into two vertically stacked panes
 byobu split-window -v
 
-#Select the newly created pane 1. Again, probably unnecessary as the new pane gets selected after a split
+# Select the newly created pane 1. Again, probably unnecessary as the new pane gets selected after a split
 byobu select-pane -t 1
 
-#Split pane 1 horizontally to create two side-by-side panes
+# Split pane 1 horizontally to create two side-by-side panes
 byobu split-window -h
 
-#Repeat the selection and splitting process with the top half
+# Repeat the selection and splitting process with the top half
 byobu select-pane -t 0
 byobu split-window -h
-#At this point, four equally sized panes have been created.
+# At this point, four equally sized panes have been created.
 
-#Send commands to each pane
+# Send commands to each pane
 for pane in 0 1 2 3; do
   byobu select-pane -t $pane
   byobu send-keys "clear; mono ttt3d.exe" Enter
