12c60065aa2f45a25f44983aba6701390b09d60d
Vitor de Paula <vitor.de.paula@consultant.volvo.com>
2024-03-05 11:12:16 +0100

    style and output improvements
    - Adjust code style
    - Print board using unicode while playing
    - Slower CPU play (50ms sleep)
    
    Signed-off-by: Vitor de Paula <vitor.de.paula@consultant.volvo.com>

--- ttt3d.cs
+++ ttt3d.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 
 class TicTacToe3D {
 
@@ -6,16 +7,13 @@ class TicTacToe3D {
     private int player; // 1: Player 1, 2: Player 2 (CPU)
     private int mask = (1 << 4) - 1;
 
-    public TicTacToe3D(int size)
-    {
-        board = new int[size, size, size];
+    public TicTacToe3D() {
+        board = new int[4, 4, 4];
         player = 1; // Start with player 1
     }
 
-    public bool PlacePiece(int x, int y, int z)
-    {
-        if (board[x, y, z] == 0)
-        {
+    public bool PlacePiece(int x, int y, int z) {
+        if (board[x, y, z] == 0) {
             board[x, y, z] = player;
             //Console.WriteLine("Player {0} placed a piece on {1}, {2}, {3}", player, x, y, z);
             player = (player == 1) ? 2 : 1; // switch sides
@@ -25,13 +23,11 @@ class TicTacToe3D {
         return false;
     }
 
-    public bool PlacePiece(int move)
-    {
+    public bool PlacePiece(int move) {
         return PlacePiece(move >> 8, (move >> 4) & mask, move & mask);
     }
 
-    public int CheckWinner()
-    {
+    public int CheckWinner() {
         // Check all cells for winning situation (this is an overkill, but no problem, board is small
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
@@ -83,41 +79,28 @@ class TicTacToe3D {
         return true;
     }
 
-    public int CPUMove()
-    {
+    public int CPUMove() {
+        // Think a bit
+        Thread.Sleep(50);
         // Prioritize winning moves (offensive)
         int move = OffensiveMove();
-        if (move != -1)
-        {
-            return move;
-        }
-
+        if (move != -1) { return move; }
         // Block opponent's winning moves (defensive)
         move = DefensiveMove();
-        if (move != -1)
-        {
-            return move;
-        }
-
+        if (move != -1) { return move; }
         // Choose a random empty cell (fallback)
         return RandomMove();
     }
 
-    private int OffensiveMove()
-    {
+    private int OffensiveMove() {
         // Check all empty cells for potential winning moves
-        for (int x = 0; x < 4; x++)
-        {
-            for (int y = 0; y < 4; y++)
-            {
-                for (int z = 0; z < 4; z++)
-                {
-                    if (board[x, y, z] == 0)
-                    {
+        for (int x = 0; x < 4; x++) {
+            for (int y = 0; y < 4; y++) {
+                for (int z = 0; z < 4; z++) {
+                    if (board[x, y, z] == 0) {
                         // Simulate placing the CPU piece at this cell
                         board[x, y, z] = player;
-                        if (CheckWinner() == player)
-                        {
+                        if (CheckWinner() == player) {
                             // Found a winning move for CPU, return it
                             board[x, y, z] = 0; // Revert the simulation
                             return (x << 8) | (y << 4) | z; // Encode the move as a single integer
@@ -127,26 +110,19 @@ class TicTacToe3D {
                 }
             }
         }
-
         return -1; // No winning move found
     }
 
-    private int DefensiveMove()
-    {
+    private int DefensiveMove() {
         // Check all empty cells for potential opponent's winning moves
-        for (int x = 0; x < 4; x++)
-        {
-            for (int y = 0; y < 4; y++)
-            {
-                for (int z = 0; z < 4; z++)
-                {
-                    if (board[x, y, z] == 0)
-                    {
+        for (int x = 0; x < 4; x++) {
+            for (int y = 0; y < 4; y++) {
+                for (int z = 0; z < 4; z++) {
+                    if (board[x, y, z] == 0) {
                         // Simulate placing opponent's piece at this cell
                         int oponnent = (player == 1) ? 2 : 1;
                         board[x, y, z] = oponnent; // Opponent's piece
-                        if (CheckWinner() == oponnent) // Check winner for opponent
-                        {
+                        if (CheckWinner() == oponnent) { // Check winner for opponent
                             // Found a potential losing move for CPU, block it
                             board[x, y, z] = 0; // Revert the simulation
                             return (x << 8) | (y << 4) | z; // Encode the move as a single integer
@@ -156,60 +132,72 @@ class TicTacToe3D {
                 }
             }
         }
-
         return -1; // No opponent's winning move found
     }
 
     private Random random = new Random();
 
-    private int RandomMove()
-    {
+    private int RandomMove() {
         // Choose a random empty cell
-        while (true)
-        {
+        while (true) {
             int x = random.Next(4);
             int y = random.Next(4);
             int z = random.Next(4);
-            if (board[x, y, z] == 0)
-            {
+            if (board[x, y, z] == 0) {
                 return (x << 8) | (y << 4) | z; // Encode the move as a single integer
             }
         }
     }
 
-    int[,,] GetBoard() {
-        return board;
+    public void DrawBoard() {
+        const char O = '\u25ef';
+        const char X = '\u2613';
+        const char vtdiv = '\u2502';
+        const char hzdiv = '\u2500';
+        const char cross = '\u253c';
+        string hline = string.Format("{0}{1}{0}{1}{0}{1}{0}", hzdiv, cross);
+        int i = 0;
+        do {
+            int j = 0;
+            do {
+               int k = 0;
+               do {
+                   int piece = board[i,j,k];
+                   Console.Write((piece == 1) ? O : ((piece == 2) ? X : ' '));
+                   if (k++ > 2) { break; }
+                   Console.Write(vtdiv);
+               } while(true);
+               if (j++ > 2) { break; }
+               Console.Write("  ");
+            } while(true);
+            Console.WriteLine();
+            if (i++ > 2) { break; }
+            Console.WriteLine("{0}  {0}  {0}  {0}", hline);
+        } while(true);
     }
 
     static void Main(string[] args) {
         // Test - put CPU to play with itself
-        TicTacToe3D game = new TicTacToe3D(4);
+        TicTacToe3D game = new TicTacToe3D();
         int winner;
+        Console.WriteLine();
+        game.DrawBoard();
         do {
             int move = game.CPUMove();
             if (!game.PlacePiece(move)) { // placing piece also switches player
                 Console.WriteLine("SOMETHING WENT WRONG, COULD NOT PLACE PIECE");
                 break;
             }
+            Console.WriteLine("\x1b[8F"); // go back 8 lines
+            game.DrawBoard();
             winner = game.CheckWinner();
         } while (winner == -1);
+        Console.WriteLine();
         switch(winner) {
             case 0: Console.WriteLine("It's a tie!"); break;
             case 1:
             case 2: Console.WriteLine("Player {0} wins!", winner); break;
             default: Console.WriteLine("ERROR! SHOULD NOT HAPPEN, GAME STILL GOING."); break;
-        }
-        int[,,] board = game.GetBoard();
-        Console.WriteLine("Board:");
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-               for (int k = 0; k < 4; k++) {
-                   int piece = board[i,j,k];
-                   Console.Write((piece == 1) ? 'O' : ((piece == 2) ? 'X' : ' '));
-               }
-               Console.Write('|');
-            }
-            Console.WriteLine();
         }
     }
 
